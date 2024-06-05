5b3c3c50a50142fd5eb34c24cfaf35c244c3ac1e
KoHeinHtetNaung <hh4883022@gmail.com>
2024-06-05 20:38:01 +0630

    mini clock

---
+++ script.js
@@ -0,0 +1,30 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+
+function updateClock () {
+    const currentDate = new Date();
+    // setTimeout(updateClock, 1000)
+
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds()
+    // console.log(hour, minute, second)
+
+    // const hourDeg = (hour / 12) * 360;
+    // hourEl.classList.transform = `rotate(${hourDeg}deg)`;
+    // const minuteDeg = (minute / 60) * 360;
+    // minuteEl.classList.transform = `rotate(${minuteDeg}deg)`;
+    // const secondDeg = (second / 60) * 360;
+    // secondEl.classList.transform = `rotate(${secondDeg}deg)`;
+    
+    const hourDeg = (hour / 12) * 360;
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    const minuteDeg = (minute / 60) * 360;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+    const secondDeg = (second / 60) * 360;
+    secondEl.style.transform = `rotate(${secondDeg}deg)`;
+
+}
+
+setInterval(updateClock, 1000)
